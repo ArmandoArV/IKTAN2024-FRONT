4dfdd8830c4c144f2da4485a1f1d9be220ec2554
ArmandoArV <armando.arredondo.valle@gmail.com>
2024-01-22 14:34:43 -0600

    Adds typing animation, changes in header

--- src/Components/HeaderComponent/HeaderComponent.tsx
+++ src/Components/HeaderComponent/HeaderComponent.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./HeaderComponent.module.css";
-import MySVG from "@/Components/SVGComponent/SVGComponent";
 import { navItems, socialMedia } from "@/Constants";
 import logo from "@/Images/Logo.png";
 import Facebook from "@/Images/Facebook.png";
@@ -12,7 +11,9 @@ export default function HeaderComponent() {
   return (
     <nav className={styles.navbar}>
       <Link href="/" passHref>
-        <img src={logo.src} alt="Logo" width={100} height={100} />
+        <img
+        className={styles.logo}
+        src={logo.src} alt="Logo" width={100} height={100} />
       </Link>
       <ul className={styles.navbarNav}>
         {navItems.map((item) => (
@@ -23,23 +24,23 @@ export default function HeaderComponent() {
           </Link>
         ))}
         <div className={styles.socialMediaContainer}>
-        {socialMedia.map((social, index) => (
-          <Link key={index} href={social.url} passHref>
-            <li className={styles.navItem}>
-              <img
-                src={
-                  social.name === "Facebook"
-                    ? Facebook.src
-                    : social.name === "Instagram"
-                    ? Instagram.src
-                    : TikTok.src
-                }
-                alt={social.name}
-                className={styles.socialMedias}
-              />
-            </li>
-          </Link>
-        ))}
+          {socialMedia.map((social, index) => (
+            <Link key={index} href={social.url} passHref>
+              <li className={styles.navItem}>
+                <img
+                  src={
+                    social.name === "Facebook"
+                      ? Facebook.src
+                      : social.name === "Instagram"
+                      ? Instagram.src
+                      : TikTok.src
+                  }
+                  alt={social.name}
+                  className={styles.socialMedias}
+                />
+              </li>
+            </Link>
+          ))}
         </div>
       </ul>
     </nav>
