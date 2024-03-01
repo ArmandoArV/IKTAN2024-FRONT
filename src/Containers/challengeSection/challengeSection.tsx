43c12a33f9b121f0af32e714283e1e23a730d2eb
armandoav <armando.arredondo.valle@gmail.com>
2024-03-01 11:23:47 -0600

    adds landing section page

---
+++ src/Containers/challengeSection/challengeSection.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ChallengeSection() {
+  return (
+    <div>
+      <h1>Challenge Section</h1>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import styles from "./page.module.css";
 import Rover from "../Images/Rover.png";
 import TeamSection from "@/Containers/teamSection/teamSection";
 import LandingSection from "@/Containers/landingSection/landingSection";
+import ChallengeSection from "@/Containers/challengeSection/challengeSection";
+
 export default function Home() {
   return (
     <main className={styles.main}>
@@ -11,6 +13,9 @@ export default function Home() {
       </section>
       <section className={styles.teamSection}>
         <TeamSection />
+      </section>
+      <section className={styles.challengeSection}>
+        <ChallengeSection />
       </section>
     </main>
   );
